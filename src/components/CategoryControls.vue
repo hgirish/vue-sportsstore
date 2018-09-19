d2b7cf4fe089e712e28c5befaf0a7a2fbd9afaa1
hgirish <girish@post.com>
2018-09-18 21:16:47 -0700

    Axios rest service

--- src/components/CategoryControls.vue
+++ src/components/CategoryControls.vue
@@ -3,22 +3,21 @@
     <div class="row my-2" v-for="c in categories" v-bind:key="c">
       <button class="btn btn-block"
       v-on:click="setCurrentCategory(c)"
-      v-bind:class="c == currentCategory ? 'btn-primary': 'btn-secondary'"
-      >
+      v-bind:class="c == currentCategory ? 'btn-primary': 'btn-secondary'">
         {{c}}
       </button>
     </div>
   </div>
 </template>
 <script>
-import {mapState, mapGetters, mapMutations} from 'vuex';
+import { mapState, mapGetters, mapMutations } from 'vuex';
 export default {
   computed: {
-    ...mapState(["currentCategory"]),
+    ...mapState(['currentCategory']),
     ...mapGetters(['categories'])
   },
   methods: {
     ...mapMutations(['setCurrentCategory'])
   }
-}
+};
 </script>
